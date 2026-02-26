7a9944a0b2c8dc7e0364715c4477d988565f50a3
ste777V6 <rapturev6.777>
2026-02-26 16:22:11 +0100

    fixed launch template week3 day2

--- week3/day2/launch-template.sh
+++ week3/day2/launch-template.sh
@@ -7,6 +7,11 @@ pwd > /home/ec2-user/install-logs.txt
 # Install Git
 sudo yum install git -y
 
+#Install python and venv 
+
+sudo yum update -y
+sudo yum install python3 python3-pip -y
+
 # Clone repository
 cd /home/ec2-user
 git clone https://github.com/ste777V6/DevopsBootcamp2026.git
