2fdad80b6204dd0d482aa2b7bdb1a26a9788e1b6
ste777V6 <rapturev6.777>
2026-09-25 10:15:24 +0200

    Update ecs - ignore chneges to task to avoid terrfaorm drift on task update

--- week6/Infra/ecs.tf
+++ week6/Infra/ecs.tf
@@ -24,6 +24,11 @@ resource "aws_ecs_task_definition" "app_task" {
   execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
   #App permission - allow to execute commands in the container
   task_role_arn = aws_iam_role.ecs_task_role.arn
+  
+  #When updating the task definition, Terrfaomr does not roll back the container definition to the previous version.(due to drift) 
+  lifecycle {
+    ignore_changes = [container_definitions]
+  }
 
   container_definitions = jsonencode([
     {
